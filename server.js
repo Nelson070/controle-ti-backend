01b4ace98a0ac1746c7a0e268a11fd205d4d4db7
Nelson070 <162140363+Nelson070@users.noreply.github.com>
2025-04-30 14:25:38 -0300

    backend 2.0

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true
 }).then(() => {
   console.log('MongoDB conectado');
-  app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }).catch(err => console.error('Erro MongoDB:', err));
