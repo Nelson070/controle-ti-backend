a3413ae84e5c3e8913abd585a6a5ad81ea50fce9
Nelson070 <162140363+Nelson070@users.noreply.github.com>
2025-04-30 14:20:54 -0300

    backend 1.5

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/api/itens', require('./routes/itens'));
 app.use('/api/usuarios', require('./routes/usuarios'));
 
-mongoose.connect('mongodb+srv://ti:69427@cluster0.n6acdwu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
